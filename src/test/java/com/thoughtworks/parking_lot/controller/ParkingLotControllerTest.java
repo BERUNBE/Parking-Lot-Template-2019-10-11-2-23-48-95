724703505c295b9c9d0c09b593b44ee2aa4d3df7
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-21 08:21:38 +0800

    Moved ParkingLotControllerTest to controller test package

--- src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
+++ src/test/java/com/thoughtworks/parking_lot/controller/ParkingLotControllerTest.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.parking_lot;
+package com.thoughtworks.parking_lot.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
